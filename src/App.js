d6578bdb8da7a40e87ea03d283b3d46ceb460d3e
Gagan Kumar <gagan761@gmail.com>
2023-12-29 17:30:17 +0530

    added 2 api as per requirement

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+
+import axios from "axios";
 
 function App() {
+  const [checkedBoxes, setCheckedBoxes] = useState({ cat: false, dog: false });
+
+  const [catFact, setCatFact] = useState("");
+
+  const [dogImage, setDogImage] = useState("");
+
+  const handleCheckboxChange = (event) => {
+    const { name, checked } = event.target;
+
+    setCheckedBoxes({ ...checkedBoxes, [name]: checked });
+
+    if (!checked) {
+      setDogImage("");
+      setCatFact("");
+    } else {
+      if (name === "cat") getCatFact();
+      if (name === "dog") getDogImage();
+    }
+  };
+
+  const getCatFact = async () => {
+    const response = await axios.get("https://catfact.ninja/fact");
+
+    setCatFact(response.data.fact);
+  };
+
+  const getDogImage = async () => {
+    const response = await axios.get("https://dog.ceo/api/breeds/image/random");
+    const res = await fetch(response.data.message);
+    const imageBlob = await res.blob();
+    const imageObjectURL = URL.createObjectURL(imageBlob);
+
+    setDogImage(imageObjectURL);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <label>
+        <h2>PICTURE MAY TAKE SOME TIME TO LOAD</h2>
+        <input
+          type="checkbox"
+          name="cat"
+          checked={checkedBoxes.cat}
+          onChange={handleCheckboxChange}
+        />
+        Cat
+      </label>
+
+      <label>
+        <input
+          type="checkbox"
+          name="dog"
+          checked={checkedBoxes.dog}
+          onChange={handleCheckboxChange}
+        />
+        Dog
+      </label>
+
+      {checkedBoxes.cat && <p>{catFact}</p>}
+
+      {checkedBoxes.dog && <img src={dogImage} alt="A random dog" />}
     </div>
   );
 }
